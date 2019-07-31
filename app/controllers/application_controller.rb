5a03a5975073255bbb0c537b04a3757cc0a4a404
aparkening <aaron.parkening@gmail.com>
2019-07-31 11:53:59 -0400

    Done. Migration and authentication successful.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,25 +16,43 @@ class ApplicationController < Sinatra::Base
     erb :signup
   end
 
+  # Create new user from username and password. If password isn't filled, user.save will return false and redirect to failure page.
   post "/signup" do
-    #your code here
-
+    if params[:username].empty? || params[:password].empty?
+      redirect "/failure"
+    else 
+      user = User.new(username: params[:username], password: params[:password])
+    end
+    
+		if user.save
+			redirect "/login"
+		else
+			redirect "/failure"
+		end
   end
 
+  # Display after successful login
   get '/account' do
     @user = User.find(session[:user_id])
     erb :account
   end
 
-
   get "/login" do
     erb :login
   end
 
+  # Find user, compare password with stored version. Redirect for success or failure.
   post "/login" do
-    ##your code here
+		user = User.find_by(username: params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/account"
+		else
+			redirect "/failure"
+		end
   end
 
+  # Display upon failed login or sign up
   get "/failure" do
     erb :failure
   end
